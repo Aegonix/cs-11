755e93eca916868cf6e36991e59dfc0202922870
prokenz101 <kenz.v@outlook.com>
2024-05-02 14:05:17 +0400

    fixed this

--- 5-2 billing mode discount.py
+++ 5-2 billing mode discount.py
@@ -9,19 +9,26 @@
 #? otherwise - No discount
 
 bill = float(input("Enter billing amount: "))
-mode = input("Enter your mode of payment: ")
+mode = input("""Enter your mode of payment: 
+c/C - Credit Card
+d/D - Debit Card
+n/N - Net Banking
+o/O - Other""")
 
-if mode.lower() == ("credit card"):
+if mode.lower() == "c":
     discount = bill / 10
     print("Your payable amount is:", (bill - discount))
 
-elif mode.lower() == ("debit card"):
+elif mode.lower() == "d":
     discount = bill / 20
     print("Your payable amount is:", (bill - discount))
 
-elif mode.lower() == ("net banking"):
+elif mode.lower() == "n":
     discount = bill / 50
     print("Your payable amount is:", (bill - discount))
 
-else:
+elif mode.lower() == "o":
     print("Your payable amount is:", bill)
+
+else:
+    print("Please enter a valid payment method.")
