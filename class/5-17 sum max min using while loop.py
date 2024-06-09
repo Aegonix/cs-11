939cf33211325e5d66f943c0e1cc08f9a71fe2fa
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2024-06-09 22:18:07 +0400

    changed to not use indexing

--- class/5-17 sum max min using while loop.py
+++ class/5-17 sum max min using while loop.py
@@ -1,25 +1,27 @@
 
-#? WAP to enter numbers as long as the user wishes.
-#? Find the sum, highest, and, lowest digit of the number entered.
+#? WAP to enter n numbers as many as the user wishes.
+#? Find the sum, highest, and, lowest digit of the numbers entered.
 
-n = int(input("Enter a positive number: "))
-max = int(str(n)[0])
+n = int(input("How many numbers? "))
+
+max = 0
 min = max
 sum = 0
-i = 0
 
-#* sum 
-while i < len(str(n)):
-    sum += int(str(n)[i])
+for i in str(n):
+    num = int(input("Enter num " + str(i + 1) + ": "))
+    sum += num
     
-    if int(str(n)[i]) > max:
-        max = int(str(n)[i])
-
-    if int(str(n)[i]) < min:
-        min = int(str(n)[i])
-
-    i += 1
-
+    if not int(i):
+        max = num
+        min = num
+    
+    else:
+        if num > max:
+            max = num
+        if num < min:
+            min = num
+        
 print("The sum of digits of", n, "is:", sum)
 print("The highest digit of", n, "is:", max)
 print("The lowest digit of", n, "is:", min)
