881cc8af45f0e2822a26ab354dd6dc9fb511482a
prokenz101 <kenz.v@outlook.com>
2024-06-09 10:15:35 +0400

    made it a bit more efficient

--- class/5-24 pattern 6.py
+++ class/5-24 pattern 6.py
@@ -5,16 +5,15 @@
 #*  CBABC
 #* DCBABCD
 
-n = int(input("Please enter the number of rows: "))
+n = int(input("Enter number of rows: "))
 
 for i in range(1, n + 1):
-    for j in range(1, n + 1 - i):
-        print(" ", end="")
+    print(" " * (n - i), end="")
+
+    for j in range(i - 1, 0, -1):
+        print(chr(65 + j), end="")
+
+    for j in range(i):
+        print(chr(65 + j), end="")
     
-    for j in range(i, 0, -1):
-        print(chr(64 + j), end="")
-
-    for j in range(2, i + 1):
-        print(chr(64 + j), end="")
-
     print()
