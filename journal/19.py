4e712ee297612eabc2ef1843307a840e98fc449c
prokenz101 <kenz.v@outlook.com>
2024-09-23 17:10:57 +0400

    fixed obvious mistake

--- journal/19.py
+++ journal/19.py
@@ -2,11 +2,10 @@
 #? Write a program to input a sentence and count the number of words in the sentence.
 
 string = input("Enter a sentence: ")
-word = input("Enter a word to count for: ")
-count = 0
+spaces_count = 1
 
-for i in range(len(string)):
-    if string[i : (len(word) + i)] == word:
-        count += 1
+for i in string:
+    if i == " ":
+        spaces_count += 1
 
-print("Number of occurences of '", word, "' is: ", count, sep="")
+print("The number of words is:", spaces_count)
