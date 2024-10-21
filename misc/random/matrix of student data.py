498c4685d1a0c0a1c99fd90001290f61d1e5e9c3
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2024-10-21 12:02:40 +0400

    added finding topper student

--- misc/random/matrix of student data.py
+++ misc/random/matrix of student data.py
@@ -11,11 +11,29 @@ for i in range(1, n + 1):
     matrix[i - 1] += [int(input("Enter the roll number of student " + str(i) + ": "))]
     matrix[i - 1] += [input("Enter the name of student " + str(i) + ": ")]
     matrix[i - 1] += [int(input("Enter the mobile number of student " + str(i) + ": "))]
+    marks = []
     for j in range(1, 6):
-        matrix[i - 1] += [float(input("Enter the marks of student " + str(i) + " for subject " + str(j) + ": "))]
-
+        marks += [float(input("Enter the marks of student " + str(i) + " for subject " + str(j) + ": "))]
+    matrix += [marks]
     print()
 
 print("The matrix is:")
 for i in matrix:
     print(i)
+
+#* finding guy with highest average
+students_avg = []
+for student in matrix:
+    sum = 0
+    for mark in student[3]:
+        sum += mark
+    
+    avg = sum / 5 #* 5 subjects
+    students_avg += [[student[1], avg]]
+
+highest_avg = [0, 0]
+for i in students_avg:
+    if i[1] > highest_avg[1]:
+        highest_avg = i
+
+print(highest_avg[0], "is the topper with an average of:", highest_avg[1])
